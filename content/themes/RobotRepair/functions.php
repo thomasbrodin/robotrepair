5c44e73d03e5ffde4af38ba4ca2dfdde725c8df3
Thomas Brodin <thomasbrodin@gmail.com>
2015-06-14 15:05:59 -0400

    Update ACF

--- content/themes/RobotRepair/functions.php
+++ content/themes/RobotRepair/functions.php
@@ -1,5 +1,8 @@
 <?php
 
+include_once('inc/tb-acf.php');
+include_once ('inc/tb-plugins.php');
+
 if ( ! class_exists( 'Timber' ) ) {
 	add_action( 'admin_notices', function() {
 			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
